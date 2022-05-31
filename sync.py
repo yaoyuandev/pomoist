40d644e2615ff0636a2f438062b2501ab8684241
Yao Yuan <yaoyuanyy@outlook.com>
2022-05-31 18:07:36 +0800

    fix: can not insert into vika when task is in the 1st lv. project (#5)
    task in the 1st lv. project do not have parent project, will cause a NPE
    
    fix #4

--- sync.py
+++ sync.py
@@ -6,5 +6,9 @@ def sync(url):
     id = int(todoist.get_id(url))
     todoist.comment('start', id)
     task = todoist.get_task(id)
+    task_root_name = ''
+    if task['root'] != None:
+        task_root_name = task['root'].name
+
     vika_api.insert(task['task'].content,
-                    task['project'].name, task['root'].name, id)
+                    task['project'].name, task_root_name, id)
